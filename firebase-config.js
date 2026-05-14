a15d55e86651915344bb35e1ea84d478ced6f98f
aldelmasquio-svg <aldelmasquio@gmail.com>
2026-05-14 12:04:58 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDvdDBTN6UUN4-Ep4qqXruFDSNee7IAzfg",
-  authDomain: "talhoes-df836.firebaseapp.com",
-  projectId: "talhoes-df836",
-  storageBucket: "talhoes-df836.firebasestorage.app",
-  messagingSenderId: "1047022702039",
-  appId: "1:1047022702039:web:001cf6a3c028c494ab8f92"
-};
